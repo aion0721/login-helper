f74df8bd0575392012ff316410e63a0fca7754e9
aion0721 <rudolf.aion@gmail.com>
2025-01-18 16:36:02 +0900

    feat: add chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,62 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+import {
+  Box,
+  Button,
+  Checkbox,
+  HStack,
+  Heading,
+  Progress,
+  RadioGroup,
+  VStack,
+} from "@chakra-ui/react";
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
+export default function Page() {
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+    <Box textAlign="center" fontSize="xl" pt="30vh">
+      <VStack gap="8">
+        <img alt="chakra logo" src="/static/logo.svg" width="80" height="80" />
+        <Heading size="2xl" letterSpacing="tight">
+          Welcome to Chakra UI v3 + Vite
+        </Heading>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+        <HStack gap="10">
+          <Checkbox.Root defaultChecked>
+            <Checkbox.HiddenInput />
+            <Checkbox.Control>
+              <Checkbox.Indicator />
+            </Checkbox.Control>
+            <Checkbox.Label>Checkbox</Checkbox.Label>
+          </Checkbox.Root>
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
+          <RadioGroup.Root display="inline-flex" defaultValue="1">
+            <RadioGroup.Item value="1" mr="2">
+              <RadioGroup.ItemHiddenInput />
+              <RadioGroup.ItemControl>
+                <RadioGroup.ItemIndicator />
+              </RadioGroup.ItemControl>
+              <RadioGroup.ItemText lineHeight="1">Radio</RadioGroup.ItemText>
+            </RadioGroup.Item>
+
+            <RadioGroup.Item value="2">
+              <RadioGroup.ItemHiddenInput />
+              <RadioGroup.ItemControl>
+                <RadioGroup.ItemIndicator />
+              </RadioGroup.ItemControl>
+              <RadioGroup.ItemText lineHeight="1">Radio</RadioGroup.ItemText>
+            </RadioGroup.Item>
+          </RadioGroup.Root>
+        </HStack>
+
+        <Progress.Root width="300px" value={65} striped>
+          <Progress.Track>
+            <Progress.Range />
+          </Progress.Track>
+        </Progress.Root>
+
+        <HStack>
+          <Button>Let's go!</Button>
+          <Button variant="outline">bun install @chakra-ui/react</Button>
+        </HStack>
+      </VStack>
+    </Box>
   );
 }
-
-export default App;
