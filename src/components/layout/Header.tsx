8393ad802f397367a364b324cb594845898ecba8
aion0721 <rudolf.aion@gmail.com>
2025-01-20 23:57:02 +0900

    fix: add logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,18 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
 import { Link as ChakraLink } from "@chakra-ui/react";
 import { Link as ReactRouterLink } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
-    <Flex as="header" bg="teal.500" color="white" p={4} justify="space-between">
-      <Box fontWeight="bold">My App</Box>
-      <Flex gap={4}>
+    <Flex as="header" bg="teal.500" color="white" p={4} align="center">
+      {/* ロゴ部分 */}
+      <Flex align="center" gap={2}>
+        <Image src="/logo.png" alt="Logo" boxSize="40px" />
+        <Text fontWeight="bold">Login Helper</Text>
+      </Flex>
+
+      {/* Spacerで左右の要素を分ける */}
+      <Flex flex="1" justify="flex-end" gap={4}>
         <ChakraLink asChild>
           <ReactRouterLink to="/">Home</ReactRouterLink>
         </ChakraLink>
