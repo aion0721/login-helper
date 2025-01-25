df5cac2a0c9655a569c211f12a9f367b8f52d75f
aion0721 <rudolf.aion@gmail.com>
2025-01-25 13:04:41 +0900

    fix: lazyload

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import React, { Suspense } from "react";
 import { Route, Routes, useLocation } from "react-router";
 import { AnimatePresence } from "framer-motion";
 import Layout from "./components/layout/Layout";
-import Home from "./components/pages/Home";
-import Config from "./components/pages/Config";
 import { AppProvider } from "./context/AppContext";
-import Server from "./components/pages/Server";
+
+const Home = React.lazy(() => import("./components/pages/Home"));
+const Config = React.lazy(() => import("./components/pages/Config"));
+const Server = React.lazy(() => import("./components/pages/Server"));
 
 const App = () => {
   const location = useLocation(); // 現在のルート情報を取得
@@ -14,11 +16,14 @@ const App = () => {
       <AnimatePresence mode="wait">
         {/* アニメーションが重ならないようにする */}
         <AppProvider>
-          <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<Home />} />
-            <Route path="/config" element={<Config />} />
-            <Route path="/server" element={<Server />} />
-          </Routes>
+          {/* Suspenseでローディング状態を管理 */}
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes location={location} key={location.pathname}>
+              <Route path="/" element={<Home />} />
+              <Route path="/config" element={<Config />} />
+              <Route path="/server" element={<Server />} />
+            </Routes>
+          </Suspense>
         </AppProvider>
       </AnimatePresence>
     </Layout>
