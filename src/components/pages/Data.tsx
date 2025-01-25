dbf7caf9721a422424356c75ec78df1689100ae9
aion0721 <rudolf.aion@gmail.com>
2025-01-25 23:58:32 +0900

    fix: add data field

--- src/components/pages/Data.tsx
+++ src/components/pages/Data.tsx
@@ -1,4 +1,6 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Button, Heading, Input, VStack } from "@chakra-ui/react";
+import { Field } from "../ui/field";
+
 import { motion } from "framer-motion";
 const Data = () => {
   return (
@@ -9,6 +11,33 @@ const Data = () => {
       transition={{ duration: 0.5 }}
     >
       <Heading>Data</Heading>
+      <Box maxW="md" mx="auto" mt={10}>
+        <VStack>
+          {/* SID フィールド */}
+          <Field label="sid">
+            <Input id="sid" placeholder="Enter SID" />
+          </Field>
+
+          <Field label="hostname">
+            <Input id="hostname" placeContent="Enter Hostname" />
+          </Field>
+
+          {/* ID フィールド */}
+          <Field label="id">
+            <Input id="id" placeholder="Enter ID" />
+          </Field>
+
+          {/* Password フィールド */}
+          <Field label="password">
+            <Input id="password" placeholder="Enter Password" />
+          </Field>
+
+          {/* Submit ボタン */}
+          <Button mt={4} colorScheme="teal" type="submit">
+            Submit
+          </Button>
+        </VStack>
+      </Box>
     </motion.div>
   );
 };
